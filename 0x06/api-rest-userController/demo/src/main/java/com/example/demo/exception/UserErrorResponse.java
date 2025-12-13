4831493458e5060f2961bd46ab26bcc13426d378
Felipe Brevio <felipebrevio05@gmail.com>
2025-12-12 21:07:12 -0300

    feat

---
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/exception/UserErrorResponse.java
@@ -0,0 +1,23 @@
+package com.example.demo.exception;
+
+public class UserErrorResponse {
+
+    private int status;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
